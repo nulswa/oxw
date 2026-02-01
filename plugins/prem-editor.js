bb41edc675d785ebd81734d324c1062f204c8119
nulswa <torusoportewa@gmail.com>
2026-02-01 04:12:07 -0300

    Update prem-editor.js

--- plugins/prem-editor.js
+++ plugins/prem-editor.js
@@ -115,7 +115,7 @@ async function getJob(jobId) {
 }
 
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!global.db.data.chats[m.chat].fPrem && m.isGroup) {
+if (!global.db.data.chats[m.chat].fPremium && m.isGroup) {
 return conn.sendMessage(m.chat, { text: `📍  Lo siento, este comando solo se utiliza al comprar un plan con premium incluído.\n\n- Usa el comando *#plan* para ver los planes disponibles.` }, { quoted: m })
 }
 

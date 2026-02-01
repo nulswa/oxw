4dff206e5cc85b461097ae9c7f45b5d34e210503
nulswa <torusoportewa@gmail.com>
2026-02-01 03:03:39 -0300

    Create index.js

---
+++ language/index.js
@@ -0,0 +1,8 @@
+import en from './en.js'; 
+import es from './es.js'; 
+import pt from './pt.js'; 
+import id from './id.js';
+import ar from './ar.js';
+import fr from './fr.js';
+
+export {en, es, pt, id, ar, fr};

73dc5846c8f2b9993f8921c893a0b5c9ad503530
nulswa <torusoportewa@gmail.com>
2026-02-05 19:38:23 -0300

    Update ps-dinfo.js

--- plugins/ps-dinfo.js
+++ plugins/ps-dinfo.js
@@ -90,7 +90,7 @@ mensaje += `📍 _Este personaje no está disponible actualmente_`;
 const imagenUrl = personaje.dfoto || personaje.pfoto;
 
 if (imagenUrl) {
-await await conn.sendMessage(m.chat, { text: mensaje, contextInfo: { forwardingScore: 1, isForwarded: false, externalAdReply: { showAdAttribution: false, renderLargerThumbnail: true, title: `${personaje.name} : ${personaje.status}`, body: textbot, containsAutoReply: true, mediaType: 1, thumbnailUrl: imagenUrl, sourceUrl: null }}}, { quoted: m })
+await await conn.sendMessage(m.chat, { text: mensaje, contextInfo: { forwardingScore: 1, isForwarded: false, externalAdReply: { showAdAttribution: false, renderLargerThumbnail: true, title: `${personaje.name} : ${personaje.status}`, body: textbot, containsAutoReply: true, mediaType: 1, thumbnailUrl: personaje.dfoto, sourceUrl: null }}}, { quoted: m })
   //conn.sendFile(m.chat, imagenUrl, 'toru.jpg', mensaje, m);
 } else {
 await conn.reply(m.chat, mensaje, m);

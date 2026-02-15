5d239d16afb32e70871a69601cf60feca8447c1f
nulswa <torusoportewa@gmail.com>
2026-02-14 22:11:41 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,16 +1,34 @@
-let handler = async (m, { conn, isAdmin }) => {
-    if (!m.isGroup) return conn.reply(m.chat, '❌ Este comando solo funciona en grupos.', m)
-    if (!isAdmin) return conn.reply(m.chat, '❌ Solo los administradores pueden usar este comando.', m)
-    
-    let chat = global.db.data.chats[m.chat]
-    
-    chat.fEnlaces = !chat.fEnlaces
-    
-    await conn.reply(m.chat, `✅ Anti enlaces ${chat.fEnlaces ? 'activado' : 'desactivado'} correctamente.`, m)
+import moment from 'moment-timezone'
+import fetch from 'node-fetch' 
+
+let handler = async (m, { conn, usedPrefix }) => {
+const file = 'https://cdn.yupra.my.id/yp/6zu4qxn5.jpg'
+let xd = `Mensaje de prueba xd`
+await conn.sendMessage(m.chat, {
+product: {
+productImage: { url: file },
+productId: '24529689176623820',
+title: botname,
+currencyCode: 'USD',
+priceAmount1000: '0',
+retailerId: 1677,
+productImageCount: 1
+},
+businessOwnerJid: m.sender,
+caption: xd.trim(),
+footer: `\n${textbot}`,
+interactiveButtons: [
+{
+name: 'cta_url',
+buttonParamsJson: JSON.stringify({
+display_text: 'tap here',
+url: 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i'
+})
+}
+],
+mentions: [m.sender]
+})
 }
 
-handler.command = ['antienlace', 'antilink', 'antilinkwa']
-handler.admin = true
-handler.group = true
-
+handler.command = ['test2']
 export default handler

91d9cb2e876bbe45ba6d3e441669de281ea2c6a0
nulswa <torusoportewa@gmail.com>
2026-02-20 01:47:02 -0300

    Update _cmdWithMedia.js

--- plugins/_cmdWithMedia.js
+++ plugins/_cmdWithMedia.js
@@ -1,4 +1,4 @@
-const { proto, generateWAMessage, areJidsSameUser } = (await import('@whiskeysockets/baileys')).default
+/*const { proto, generateWAMessage, areJidsSameUser } = (await import('@whiskeysockets/baileys')).default
 
 export async function all(m, chatUpdate) {
 if (m.isBaileys) return
@@ -23,4 +23,4 @@ type: 'append'
 }
 this.ev.emit('messages.upsert', msg)
 }
-
+*/

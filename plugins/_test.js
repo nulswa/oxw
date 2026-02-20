5822cf6edd972f490f8b279d9f2dd218d3cfeb53
nulswa <torusoportewa@gmail.com>
2026-02-20 02:20:31 -0300

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,34 +1,78 @@
-import moment from 'moment-timezone'
-import fetch from 'node-fetch' 
+import pkg from '@whiskeysockets/baileys'
+const {generateWAMessageFromContent, proto} = pkg
 
-let handler = async (m, { conn, usedPrefix }) => {
-const file = 'https://cdn.yupra.my.id/yp/6zu4qxn5.jpg'
-let xd = `Mensaje de prueba xd`
-await conn.sendMessage(m.chat, {
-product: {
-productImage: { url: file },
-productId: '24529689176623820',
-title: botname,
-currencyCode: 'USD',
-priceAmount1000: '0',
-retailerId: 1677,
-productImageCount: 1
+var handler = async (m, {conn, usedPrefix}) => {
+let msg = generateWAMessageFromContent(
+m.chat,
+{
+viewOnceMessage: {
+message: {
+messageContextInfo: {
+deviceListMetadata: {},
+deviceListMetadataVersion: 2
 },
-businessOwnerJid: m.sender,
-caption: xd.trim(),
-footer: `\n${textbot}`,
-interactiveButtons: [
+interactiveMessage: proto.Message.InteractiveMessage.create({
+body: proto.Message.InteractiveMessage.Body.create({
+text: 'test'
+}),
+footer: proto.Message.InteractiveMessage.Footer.create({
+text: 'test'
+}),
+header: proto.Message.InteractiveMessage.Header.create({
+title: 'test',
+subtitle: 'test',
+hasMediaAttachment: false
+}),
+nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+buttons: [
+{
+name: 'single_select',
+buttonParamsJson:
+'{"title":"title","sections":[{"title":"title","highlight_label":"label","rows":[{"header":"header","title":"title","description":"description","id":"id"},{"header":"header","title":"title","description":"description","id":"id"}]}]}'
+},
+{
+name: 'quick_reply',
+buttonParamsJson: '{"display_text":"quick_reply","id":"#s"}'
+},
 {
 name: 'cta_url',
-buttonParamsJson: JSON.stringify({
-display_text: 'tap here',
-url: 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i'
+buttonParamsJson: '{"display_text":"url","url":"https://www.google.com","merchant_url":"https://www.google.com"}'
+},
+{
+name: 'cta_call',
+buttonParamsJson: '{"display_text":"call","id":"message"}'
+},
+{
+name: 'cta_copy',
+buttonParamsJson: '{"display_text":"copy","id":"123456789","copy_code":"message"}'
+},
+{
+name: 'cta_reminder',
+buttonParamsJson: '{"display_text":"cta_reminder","id":"message"}'
+},
+{
+name: 'cta_cancel_reminder',
+buttonParamsJson: '{"display_text":"cta_cancel_reminder","id":"message"}'
+},
+{
+name: 'address_message',
+buttonParamsJson: '{"display_text":"address_message","id":"message"}'
+},
+{
+name: 'send_location',
+buttonParamsJson: ''
+}
+]
+})
 })
 }
-],
-mentions: [m.sender]
-})
 }
+},
+{}
+)
 
-handler.command = ['test']
+await conn.relayMessage(msg.key.remoteJid, msg.message, {messageId: msg.key.id})
+}
+handler.command = ["mboton"]
+
 export default handler

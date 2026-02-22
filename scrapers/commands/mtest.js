85ab71c95cd7e6a52c7e615db1fcc61672d1cc7f
nulswa <torusoportewa@gmail.com>
2026-02-22 03:50:24 -0300

    Update mtest.js

--- scrapers/commands/mtest.js
+++ scrapers/commands/mtest.js
@@ -1,6 +1,6 @@
 let handler = async (m, { text, usedPrefix, command }) => {
 let mensajito = `Mensaje directo en .../scrapers/commands`;
-await conn.sendMessage(m.chat, { text: mensajito }, { quoted: 
+await conn.sendMessage(m.chat, { text: mensajito }, { quoted: m })
 };
 handler.command = ["mtest"];
 export default handler
